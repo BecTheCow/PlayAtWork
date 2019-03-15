ca4007ad1625b31ae11ff1bba8777e76e3826f42
Rebecca Grindley <Rebecca@Headquarters.7Layer.net>
2019-03-15 11:23:26 +0000

    setting dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:protractor/recommended",
+        "plugin:cucumber"
+    ],
+
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+    }
+};
